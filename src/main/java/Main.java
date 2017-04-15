35035fd7dfdae1d12a84953e69890417cfaab43c
khoroshkovkirill <khoroshkov_kirill@mail.ru>
2017-04-15 16:47:50 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,68 @@
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+import java.io.IOException;
+
+public class Main {
+
+    @Argument(required = true, usage = "Command")
+    private String command;
+
+    @Option(name = "-d",usage = "Выходные файлы следует называть “ofile1, ofile2, ofile3, ofile4 …”")
+    private boolean numberFormat;
+
+    @Option(name = "-l",usage = "Размер выходных файлов в строчках")
+    private boolean sizeInLines;
+
+    @Option(name = "-c",usage = "Размер выходных файлах в символах")
+    private boolean sizeInChars;
+
+    @Option(name = "-n",usage = "Количество выходных файлов")
+    private boolean countOfFiles;
+
+    @Argument(required = false, index = 1, usage = "Размер выходного файла")
+    private int size;
+
+    @Option(name = "-o",usage = "Задаёт базовое имя выходного файла")
+    private boolean ofile;
+
+    @Argument(required = false, index = 2, usage = "Имя выходного файла")
+    private String nameOfOutFiles;
+
+    @Argument(required = true, index = 3, usage = "Имя входного файла")
+    private String nameOfInFile;
+
+    public static void main(String[] args) throws IOException {
+        try {
+            new Main().doSplit(args);
+            System.exit(0);
+        }
+        catch (IllegalArgumentException ex){
+            System.out.println(ex.getMessage());
+            System.exit(65);
+        }
+        catch (IOException ex){
+            System.out.println("Ошибка чтения файла");
+            System.exit(74);
+        }
+        catch (CmdLineException ex){
+            System.out.println("Ошибка чтения командной строки");
+            System.exit(64);
+        }
+        catch (Exception ex){
+            System.out.println("Беда");
+            System.exit(-1);
+        }
+    }
+
+    private void doSplit(String[] args) throws CmdLineException, IOException {
+        CmdLineParser parser = new CmdLineParser(this);
+        parser.parseArgument(args);
+        Split splitObj = new Split(numberFormat, sizeInLines, sizeInChars, countOfFiles,
+                size, ofile, nameOfOutFiles, nameOfInFile);
+        splitObj.writeInFiles();
+    }
+
+}
---
+++ src/main/java/Split.java
@@ -0,0 +1,98 @@
+import java.io.*;
+
+/**
+ * Created by khoroshkovkirill on 01.04.17.
+ */
+final class Split {
+    final private boolean numberFormat;
+    final private boolean sizeInLines;//\
+    final private boolean sizeInChars;//-одно из трех
+    final private boolean countOfFiles;//
+    final private int size;
+    final private boolean ofile;
+    final private String nameOfOutFiles;
+    final private String nameOfInFile;
+
+    public Split(boolean numberFormat, boolean sizeInLines, boolean sizeInChars, boolean countOfFiles,
+                 int size, boolean ofile, String nameOfOutFiles, String nameOfInFile) {
+        if (sizeInChars && sizeInLines || sizeInChars && countOfFiles || sizeInLines && countOfFiles){
+            throw new IllegalArgumentException("Недопустимое сочетание флагов управления размером");
+        }
+        if ((sizeInChars || sizeInLines || countOfFiles) == (size == 0)){
+            throw new IllegalArgumentException("Указан размер но не указан флаг управления размером(или наоборот)");
+        }
+        if (ofile == (nameOfOutFiles == null)){
+            throw new IllegalArgumentException("Отсутствует флаг -o, но указано имя выходного файла(или наоборот)");
+        }
+        this.numberFormat = numberFormat;
+        this.sizeInLines = sizeInLines;
+        this.sizeInChars = sizeInChars;
+        this.countOfFiles = countOfFiles;
+        this.size = size;
+        this.ofile = ofile;
+        this.nameOfOutFiles = nameOfOutFiles;
+        this.nameOfInFile = nameOfInFile;
+    }
+
+    public void writeInFiles() throws IOException{
+        //Название выходных файлов
+        String out;
+        if (!this.ofile){
+            out = "x";
+        }
+        else{
+            if (this.nameOfOutFiles.equals("-")){
+                out = this.nameOfInFile;
+            }
+            else{
+                out = this.nameOfOutFiles;
+            }
+        }
+        //Чтение и запись
+        if (this.sizeInLines) writeInFilesIfSizeInLines(out, this.size);
+        if (this.countOfFiles) writeInFilesIfSizeInChars(out, this.lengthOfFile(nameOfInFile) / this.size + 1);
+        if (this.sizeInChars) writeInFilesIfSizeInChars(out, this.size);
+        if (this.size == 0) writeInFilesIfSizeInLines(out, 100);
+    }
+
+    public void writeInFilesIfSizeInLines(String out, int size) throws IOException{
+        BufferedReader br = new BufferedReader(new FileReader(this.nameOfInFile));
+        String str;
+        int number = 0;
+        Integer numFile = 1;
+        BufferedWriter bw = new BufferedWriter(new FileWriter(out + "1"));
+        while ((str = br.readLine()) != null) {
+            number++;
+            if (numFile == number / size + 1) {
+                numFile = number / size + 1;
+                bw = new BufferedWriter(new FileWriter(out + numFile.toString()));
+            }
+            bw.write(str);
+        }
+    }
+
+    public void writeInFilesIfSizeInChars(String out, int size) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(this.nameOfInFile));
+        int ch;
+        int number = 0;
+        Integer numFile = 1;
+        BufferedWriter bw = new BufferedWriter(new FileWriter(out + "1"));
+        while ((ch = br.read()) != -1) {
+            number++;
+            if (numFile == number / size + 1) {
+                numFile = number / size + 1;
+                bw = new BufferedWriter(new FileWriter(out + numFile.toString()));
+            }
+            bw.write(ch);
+        }
+    }
+
+    public int lengthOfFile(String nameOfFile) throws IOException{
+        int length = 0;
+        BufferedReader br = new BufferedReader (new FileReader(nameOfFile));
+        while(br.read()!=-1){
+            length++;
+        }
+        return length;
+    }
+}
